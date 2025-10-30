6b00943ac7ce45dfb9c2a976564ada8b3b7541cf
JASSNexocorp <JSAHONERO@farmacorp.com>
2025-10-29 23:22:32 -0400

    Mover workflow a .github/workflows

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/hola', (req, res) => {
-    res.json({ mensaje: 'Hola Mundo 2' });
+    res.json({ mensaje: 'Hola Mundo 22' });
 });
 
 export default app;
